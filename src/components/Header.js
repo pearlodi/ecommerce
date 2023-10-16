5d1aade5b16a961f6f129c5831ec61267392dad3
pearlodi7@gmail.com <you@example.com>
2023-10-16 16:04:28 +0100

    project setup

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import  '../assets/css/header.css'
+
+
+const Header = () => {
+  return (
+    <div>
+      <div className='head'>
+          <h1>hi</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header
